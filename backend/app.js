1ab3bcef14d16d2e4d18fc99e57e939f84e97726
rigole <foplacide@gmail.com>
2022-03-25 23:31:32 +0100

    setting middleware for post request

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,6 +9,12 @@ app.use((req, res, next) => {
     next();
 });
 
+app.post('api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message:'Object Created'
+    });
+});
 
 
 
