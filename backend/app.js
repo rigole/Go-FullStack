ff75adafdf006a05ddc870af182a1b59849eaac3
rigole <foplacide@gmail.com>
2022-03-25 22:00:49 +0100

    Testing middlewares

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: "Server Started" })
+app.use((req, res, next) => {
+    console.log("Successfull Request");
+    next();
 });
 
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({ message: "Server Started" });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log(" response send successfully")
+})
 module.exports = app;
