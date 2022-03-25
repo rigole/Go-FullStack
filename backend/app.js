2635d0d7483cd0880dbdd7d92721c98d96ac8184
rigole <foplacide@gmail.com>
2022-03-25 22:52:23 +0100

    setting the new API

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,31 @@
 const express = require('express');
 
+
+
+
+
+
+
 const app = express();
-
-app.use((req, res, next) => {
-    console.log("Successfull Request");
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: "Server Started" });
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log(" response send successfully")
-})
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 module.exports = app;
