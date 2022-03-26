d8c4c95a327646b0fd5ed3cc0cdef682f0450c71
rigole <foplacide@gmail.com>
2022-03-26 16:01:34 +0100

    setting the backend for saving and getting data

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Thing = require('./models/thing');
+
 app.use(express.json())
 
 const MONGO_URL = 'mongodb+srv://plass:123@nasa-cluster.exsxa.mongodb.net/nasaDatabase?retryWrites=true&w=majority'
@@ -20,12 +22,17 @@ app.use((req, res, next) => {
     next();
 });
 
+
+
+
 app.post('api/stuff', (req, res, next) => {
     console.log(req.body);
     res.status(201).json({
         message:'Object Created'
     });
 });
+
+
 
 
 
@@ -53,4 +60,29 @@ app.use('/api/stuff', (req, res, next) => {
     ];
     res.status(200).json(stuff);
   });
+
+
+
+  app.post('api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
+    });
+    thing.save()
+    .then(() => res.status(201).json({message: 'Saved Object'}))
+    .catch(error => res.status(400).json({ error }))
+  })
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }))
+})
+
+  app.use('api/stuff', (req, res, next) => {
+    Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }))
+  })
+
 module.exports = app;
